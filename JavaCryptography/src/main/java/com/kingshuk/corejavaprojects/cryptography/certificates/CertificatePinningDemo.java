bff0287564b40428e44a53bed304902391547ebb
Kingshuk Mukherjee <sly.mania42@gmail.com>
2021-04-26 21:43:25 -0500

    certificate pinning

---
+++ JavaCryptography/src/main/java/com/kingshuk/corejavaprojects/cryptography/certificates/CertificatePinningDemo.java
@@ -0,0 +1,72 @@
+package com.kingshuk.corejavaprojects.cryptography.certificates;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+
+public class CertificatePinningDemo {
+    private static final Logger logger = LoggerFactory.getLogger(CertificatePinningDemo.class);
+
+    public static void main(String[] args) throws Exception {
+        final String hostname = "https://www.udemy.com";
+
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+
+        KeyManager [] keyManagers = {};
+
+        final TrustManagerFactory trustManagerFactory =
+                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+
+        trustStore.load(CertificatePinningDemo.class.getResourceAsStream("keystore"), "changeit".toCharArray());
+
+        trustManagerFactory.init(trustStore);
+
+        sslContext.init(keyManagers, trustManagerFactory.getTrustManagers(), new SecureRandom());
+
+        final URL url = new URL(hostname);
+
+        final HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+
+        urlConnection.setSSLSocketFactory(sslContext.getSocketFactory());
+
+        connectAndValidate(urlConnection);
+
+    }
+
+    private static void connectAndValidate(HttpsURLConnection urlConnection){
+        try{
+            urlConnection.connect();
+            logger.info("Connected...");
+            readBytes(urlConnection);
+        }catch (SSLHandshakeException e) {
+           logger.error("Certificate pin missing", e);
+        } catch (SSLException e) {
+            logger.error("Unable to access valid keystore", e);
+        } catch (IOException e) {
+            logger.error("Something went wrong", e);
+        }
+    }
+
+    private static void readBytes(HttpsURLConnection urlConnection) {
+        try (InputStream in = urlConnection.getInputStream()) {
+            final ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            final byte[] bytes = new byte[1024];
+
+            //I don't want to read the full html but this will do it.
+            final int length = in.read(bytes);
+            bout.write(bytes, 0, length);
+           logger.info(bout.toString());
+        } catch (IOException e) {
+            logger.error("Something went wrong", e);
+        }
+    }
+}
